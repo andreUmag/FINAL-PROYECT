948fde40fa916ea2104c662811610de37c03f392
andreUmag <142335765+andreUmag@users.noreply.github.com>
2023-11-18 20:52:43 -0500

    formato a los documentos

--- static/scripts/speech.js
+++ static/scripts/speech.js
@@ -1,37 +1,37 @@
 if ("speechSynthesis" in window) {
-    const speech = new SpeechSynthesisUtterance();
+  const speech = new SpeechSynthesisUtterance();
 
-    document.addEventListener('languageChange', function (event) {
-        const lang = event.detail;
-        setSpeechLanguage(lang);
-    });
+  document.addEventListener("languageChange", function (event) {
+    const lang = event.detail;
+    setSpeechLanguage(lang);
+  });
 
-    function setSpeechLanguage(lang) {
-        switch (lang) {
-            case "en":
-                speech.lang = "en-US";
-                break;
-            case "es":
-                speech.lang = "es-ES";
-                break;
-            case "jp":
-                speech.lang = "ja-JP";
-                break;
-            default:
-                speech.lang = "en-US";
-                break;
-        }
+  function setSpeechLanguage(lang) {
+    switch (lang) {
+      case "en":
+        speech.lang = "en-US";
+        break;
+      case "es":
+        speech.lang = "es-ES";
+        break;
+      case "jp":
+        speech.lang = "ja-JP";
+        break;
+      default:
+        speech.lang = "en-US";
+        break;
     }
+  }
 
-    function speak(text) {
-        speech.text = text;
-        speechSynthesis.speak(speech);
-    }
+  function speak(text) {
+    speech.text = text;
+    speechSynthesis.speak(speech);
+  }
 
-    // prueba
-    document.getElementById("execute").addEventListener("click", function () {
-        speak("Reproduciendo mensaje al hacer clic.");
-    });
+  // prueba
+  document.getElementById("execute").addEventListener("click", function () {
+    speak("Reproduciendo mensaje al hacer clic.");
+  });
 } else {
-    console.log("La síntesis de voz no es compatible con este navegador.");
+  console.log("La síntesis de voz no es compatible con este navegador.");
 }
--- static/scripts/translations.js
+++ static/scripts/translations.js
@@ -1,34 +1,36 @@
 document.addEventListener("DOMContentLoaded", function () {
-    let currentLanguage = "en";
+  let currentLanguage = "en";
 
-    function changeLanguage(lang) {
-        currentLanguage = lang;
-        fetch('/static/languages/' + lang + '.json')
-            .then(response => response.json())
-            .then(data => {
-                document.title = data.title;
-                document.querySelector('#languages a').textContent = data.languages;
-                document.querySelector('#interpreter').textContent = data.interpreter;
-                document.querySelector('#web').textContent = data.web;
-                document.querySelector('#execute').textContent = data.execute;
-                document.querySelector('#validate').textContent = data.validate;
+  function changeLanguage(lang) {
+    currentLanguage = lang;
+    fetch("/static/languages/" + lang + ".json")
+      .then((response) => response.json())
+      .then((data) => {
+        document.title = data.title;
+        document.querySelector("#languages a").textContent = data.languages;
+        document.querySelector("#interpreter").textContent = data.interpreter;
+        document.querySelector("#web").textContent = data.web;
+        document.querySelector("#execute").textContent = data.execute;
+        document.querySelector("#validate").textContent = data.validate;
 
-                const languageOptions = data["languages-option"];
-                document.querySelectorAll('.language-list a').forEach(link => {
-                    const newLang = link.getAttribute('data-lang');
-                    link.textContent = languageOptions[newLang];
-                });
-
-                const languageChangeEvent = new CustomEvent('languageChange', { detail: lang });
-                document.dispatchEvent(languageChangeEvent);
-            });
-    }
-
-    document.querySelectorAll('.language-list a').forEach(link => {
-        link.addEventListener('click', function (event) {
-            event.preventDefault();
-            const lang = this.getAttribute('data-lang');
-            changeLanguage(lang);
+        const languageOptions = data["languages-option"];
+        document.querySelectorAll(".language-list a").forEach((link) => {
+          const newLang = link.getAttribute("data-lang");
+          link.textContent = languageOptions[newLang];
         });
+
+        const languageChangeEvent = new CustomEvent("languageChange", {
+          detail: lang,
+        });
+        document.dispatchEvent(languageChangeEvent);
+      });
+  }
+
+  document.querySelectorAll(".language-list a").forEach((link) => {
+    link.addEventListener("click", function (event) {
+      event.preventDefault();
+      const lang = this.getAttribute("data-lang");
+      changeLanguage(lang);
     });
+  });
 });
