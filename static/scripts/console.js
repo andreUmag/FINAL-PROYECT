fab67f987f1fe94d6cd38ce9e9162fa1532ce0b4
andreUmag <142335765+andreUmag@users.noreply.github.com>
2023-11-19 22:10:26 -0500

    Reduccion console

--- static/scripts/console.js
+++ static/scripts/console.js
@@ -1,17 +1,10 @@
 (function () {
-  var oldConsoleLog = console.log;
   var outputDiv = document.querySelector(".console");
 
   console.log = function (message) {
-    if (typeof message == "object") {
-      outputDiv.innerHTML +=
-        (JSON && JSON.stringify ? JSON.stringify(message) : message) + "<br>";
-    } else {
-      outputDiv.innerHTML += message + "<br>";
-    }
-    oldConsoleLog.apply(console, arguments);
+    outputDiv.innerHTML +=
+      (JSON && JSON.stringify ? JSON.stringify(message) : message) + "<br>";
   };
 })();
 
-console.log("Hola, esto se mostrará en el div.");
-console.log(5+5);
+console.log("Hola mundo")
