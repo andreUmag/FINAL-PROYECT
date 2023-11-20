ca3bc305a00ac2739796b7cb7c6ff4db39311926
andreUmag <142335765+andreUmag@users.noreply.github.com>
2023-11-20 13:56:22 -0500

    mostrar consola

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, request, jsonify
+import subprocess
+
 
 app = Flask(__name__)
 
@@ -16,6 +18,17 @@ def guardar_texto():
         file.write(texto)
 
     return jsonify({'mensaje': 'Texto guardado exitosamente'})
+
+@app.route('/execute_code')
+def execute_code():
+    try:
+        # Ejecutar el código desde el archivo principal.py y capturar la salida
+        result = subprocess.check_output(['python', 'backend/ply/principal.py'], universal_newlines=True)
+
+        # Devolver la salida como JSON
+        return jsonify({'mensaje': result})
+    except Exception as e:
+        return jsonify({'mensaje': f'Error: {str(e)}'})
 
 if __name__ == '__main__':
     app.run(debug=True)
